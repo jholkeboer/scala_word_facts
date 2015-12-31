a1e9c16526f79971c09f21cd91f82fb5950b9f0d
jholkeboer <jholkeboer@users.noreply.github.com>
2015-12-31 10:43:22 -0800

    remove punctuation

--- str_proc.scala
+++ str_proc.scala
@@ -1,7 +1,13 @@
 //val sentence = readLine("Enter a sentence: ")
 val sentence = "The quick brown fox jumps over the lazy dog."
+try {
+  if (sentence.length == 0) throw new IllegalArgumentException("Sentence was empty")
+} catch {
+  case _: IllegalArgumentException => println("Sentence was empty.")
+}
+
 val words = sentence.split(" ")
-//val words_clean = words map { w => if w.contains(".") }
+val words_clean = words map { w => w filter (l => l != '.' && l != ',') }
 println("Your sentence: " + sentence)
 
 print("Your sentence in reverse: ")
@@ -11,5 +17,7 @@ println("Characters in your sentence: " + sentence.length)
 
 println("Words in your sentence: " + words.length)
 
-val avg_word_len = { var sum = 0; words.map {w => sum = sum + w.length}; sum/words.length }
-printf("Average word length: %d" + '\n', avg_word_len)
+val avg_word_len = { var sum = 0; words_clean.map {w => sum = sum + w.length}; sum/words.length }
+printf("Average word length: %d" + '\n' + '\n', avg_word_len)
+
+println("Frequency of each word: ")
