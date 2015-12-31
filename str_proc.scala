132f72d4ddb87ac22cd3ec9f56597087da041b00
jholkeboer <jholkeboer@users.noreply.github.com>
2015-12-31 09:02:09 -0800

    avg word len

--- str_proc.scala
+++ str_proc.scala
@@ -1,1 +1,15 @@
-val myWord = readLine("Enter a word: ")
+//val sentence = readLine("Enter a sentence: ")
+val sentence = "The quick brown fox jumps over the lazy dog."
+val words = sentence.split(" ")
+//val words_clean = words map { w => if w.contains(".") }
+println("Your sentence: " + sentence)
+
+print("Your sentence in reverse: ")
+words.reverse map {w => print(w + " ")} ; print('\n')
+
+println("Characters in your sentence: " + sentence.length)
+
+println("Words in your sentence: " + words.length)
+
+val avg_word_len = { var sum = 0; words.map {w => sum = sum + w.length}; sum/words.length }
+printf("Average word length: %d" + '\n', avg_word_len)
